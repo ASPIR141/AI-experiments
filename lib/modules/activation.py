56be8895e76b535379e9028b8a5474db0267c533
Diana Malyk <scintiamon@gmail.com>
2020-11-27 12:17:37 +0200

    Refactor

--- lib/modules/activation.py
+++ lib/modules/activation.py
@@ -3,38 +3,38 @@ from torch.tensor import Tensor
 
 
 def hardmax(X_in: Tensor) -> Tensor:
-  r"""hardmax(X_in) -> Tensor
+    r"""hardmax(X_in) -> Tensor
 
-  Applies hardmax function element-wise.
-  Elements will be probability distributions that sum to 1
-  and in such a way, that makes it possible to avoid unnecessary
-  non-linearity effect typical to the Softmax procedure.
+    Applies hardmax function element-wise.
+    Elements will be probability distributions that sum to 1
+    and in such a way, that makes it possible to avoid unnecessary
+    non-linearity effect typical to the Softmax procedure.
 
-  Args:
-    X_in (Tensor): the input tensor.
+    Args:
+        X_in (Tensor): the input tensor.
 
-  Returns:
-    A tensor of the same shape as input.
-  """
+    Returns:
+        A tensor of the same shape as input.
+    """
 
-  x = X_in.clone()
-  m = torch.sum(torch.abs(x), dim=1, keepdim=True)
-  for idx, sum_i in enumerate(m):
-    if sum_i == 0:
-      m[idx] = 1
-      x[idx][x[idx] == 0] = 1
-  s = x.add(m) / (2 * m)
-  output = s / torch.sum(s, dim=1, keepdim=True)
-  return output
+    x = X_in.clone()
+    m = torch.sum(torch.abs(x), dim=1, keepdim=True)
+    for idx, sum_i in enumerate(m):
+        if sum_i == 0:
+            m[idx] = 1
+            x[idx][x[idx] == 0] = 1
+    s = x.add(m) / (2 * m)
+    output = s / torch.sum(s, dim=1, keepdim=True)
+    return output
 
 
 def hardsquaremax(X_in: Tensor) -> Tensor:
-  x = X_in.clone()
-  m = torch.sqrt(torch.sum((torch.pow(x, 2)), dim=1, keepdim=True))
-  for idx, sum_i in enumerate(m):
-    if sum_i == 0:
-      m[idx] = 1
-      x[idx][x[idx] == 0] = 1
-  s = x.add(m) / (2 * m)
-  output = s / torch.sum(s, dim=1, keepdim=True)
-  return output
+    x = X_in.clone()
+    m = torch.sqrt(torch.sum((torch.pow(x, 2)), dim=1, keepdim=True))
+    for idx, sum_i in enumerate(m):
+        if sum_i == 0:
+            m[idx] = 1
+            x[idx][x[idx] == 0] = 1
+    s = x.add(m) / (2 * m)
+    output = s / torch.sum(s, dim=1, keepdim=True)
+    return output
