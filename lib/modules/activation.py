Fixed z-score normalization and updated model

lib/modules/activation.py
@@ -46,9 +46,9 @@ def z_score(logits: Tensor) -> Tensor:
     '''
     assert(len(logits.shape) >= 2)
 
-    mean = torch.mean(logits)
+    mean = torch.mean(logits, dim=1, keepdim=True)
     deviation = torch.sqrt(
-        torch.sum(torch.pow(logits - mean, 2))/logits.size()[1])
+        torch.sum(torch.pow(logits - mean, 2), dim=1, keepdim=True)/logits.size()[1])
     z_norm = (logits - mean)/deviation
     return z_norm
 

training/cnn/training_loop.py
@@ -21,11 +21,11 @@ def training_loop(batch_size, epochs, gamma, seed, log_interval, save_model):
         print('CUDA support is enabled')
 
     ngpu = torch.cuda.device_count()
-    device = torch.device("cuda:0" if (use_cuda and ngpu > 0) else "cpu")
+    device = torch.device('cuda:0' if (use_cuda and ngpu > 0) else 'cpu')
 
     transform=transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize((0.1307,), (0.3081,))
+        transforms.Normalize((0.5), (0.5))
     ])
 
     trainset = datasets.MNIST("./assets/data/mnist", train=True, download=True, transform=transform)