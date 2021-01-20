c1b2164170d0c310c2cf3459611673f68d599232
Diana Malyk <scintiamon@gmail.com>
2021-01-20 23:43:29 +0200

    Add naive confusion

--- lib/modules/layers.py
+++ lib/modules/layers.py
@@ -35,6 +35,38 @@ def calculate_probabilities(array: FloatTensor, v: FloatTensor, tax: FloatTensor
                 probabilities.append(torch.tensor(0., device=device) if i == 1 else ((i * j) / (1 - i)) * tax)
     return torch.stack(probabilities)
 
+def naive_confusion_layer(p: Tensor, l: List, k: int) -> Tuple[List[float], List[str]]:
+    """
+    Arguments:
+        p: Tensor, probabilities from softmax layer.
+        l: List, list of labels.
+        k: Integer, a number of classes.
+
+    Returns:
+        probabilities, labels: Tuple[List[Float], List[Str]]
+    """
+    device = p.device
+
+    v = p.std(dim=1, unbiased=True)*k
+    tax = 1 - v
+
+    v, t = v.to(device), tax.to(device)
+
+    # create all possible combinations and represent each combination as a tensor to find the diagonal elements
+    # tensor([0., 1.]) -> tensor([[[0., 0.],[0., 1.]], [[1., 0.],[1., 1.]]])
+    c = [torch.tensor(list(combinations(x, 2)), dtype=torch.float).view(k, -1, 2).to(device) for x in p]
+
+    labels = ([f'{x}_{y}' for x, y in product(l, l)])
+
+    # calculate probabilities for each combination
+    probabilities = [calculate_probabilities(c, v[idx], tax[idx]) for idx, c in enumerate(c)]
+
+    # get indexes of the max probabilities, f.e. [tensor([0.1200, 0.0800, 0.3200, 0.4800]), tensor([0.1200, 0.4800, 0.3200, 0.0800])]
+    max_probabilities_indexes = [torch.argmax(x).item() for x in probabilities]
+    labels = [labels[i] for i in max_probabilities_indexes]
+    probabilities = [round(torch.max(x).item(), 2) for x in probabilities]
+    return probabilities, labels
+
 def confusion_layer(p: Tensor, l: List, k: int) -> Tuple[List[float], List[str]]:
     """
     Arguments:
